5f82e70f02b46b6e35f2798a543de88a262ea86e
Will Woods <will@profian.com>
2021-09-13 12:16:43 -0400

    WIP: make this crate library that includes a binary
    We want to allow other code (like a client/server) to use
    `enarx_keepldr` as a library to build keeps, rather than having to
    `exec()` the `enarx-keepldr` binary.
    
    This commit refactors the crate to be a library - with an included
    `enarx-keepldr` binary, built from `src/bin/enarx-keepldr.rs`.
    
    This is a work-in-progress - the documentation on the `enarx_keepldr`
    library is incomplete. There are some half-assed placeholder doc
    comments here and there, but in order to get this to compile (so I can
    make sure everything still works as expected) I've temporarily turned on
    the #[allow(missing_docs)] directive.
    
    Signed-off-by: Will Woods <will@profian.com>

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
+//! # Backend
+//! TODO: this is a placeholder for proper module documentation
+
 #[cfg(feature = "backend-kvm")]
 pub mod kvm;
 
@@ -15,6 +18,9 @@ use std::sync::Arc;
 use anyhow::Result;
 use sallyport::Block;
 
+/// The Backend trait is an abstraction over the various hardware TEE backends
+/// (Intel SGX, AMD SEV, and so on).
+/// TODO: explain better
 pub trait Backend {
     /// The name of the backend
     fn name(&self) -> &'static str;
@@ -34,6 +40,7 @@ pub trait Backend {
     fn build(&self, shim: Component, code: Component) -> Result<Arc<dyn Keep>>;
 }
 
+/// A single piece of data about the host's support for a given Backend.
 pub struct Datum {
     /// The name of this datum.
     pub name: String,
@@ -48,19 +55,35 @@ pub struct Datum {
     pub mesg: Option<String>,
 }
 
+/// The `Keep` trait gives an interface for spawning a Thread inside a Keep.
+/// (TODO: more docs...)
 pub trait Keep {
     /// Creates a new thread in the keep.
     fn spawn(self: Arc<Self>) -> Result<Option<Box<dyn Thread>>>;
 }
 
+/// The `Thread` trait enters the Thread in the Keep and then returns a Command,
+/// which indicates why the thread has paused/ceased execution and what we
+/// need to do about it. See Command for details.
+/// TODO: I made this up; someone should edit/approve it
+/// TODO: Link "Command" to the `Command` enum
 pub trait Thread {
     /// Enters the keep.
     fn enter(&mut self) -> Result<Command>;
 }
 
+/// The Command enum gives the reason we stopped execution of the Thread, and
+/// tells us what to do next - either we need to handle a Syscall or we can
+/// simply Continue on our way.
+/// TODO: uhhh I made that explanation up, someone should rewrite/verify this..
 pub enum Command<'a> {
+    /// This indicates that we need to handle a SysCall.
+    /// TODO: ...or does it mean we just handled one?
+    /// TODO: also, explain Block?
     #[allow(dead_code)]
     SysCall(&'a mut Block),
+
+    /// No need to handle a SysCall, we can just continue on our way
     #[allow(dead_code)]
     Continue,
 }
--- src/binary/mod.rs
+++ src/binary/mod.rs
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
+//! # Binary
+//! TODO: fill in docs for this module
+
 mod component;
 
 pub use component::*;
